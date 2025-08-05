d0ef7eae0490353add0a8dd2e3f0f76c465bf9cc
595554963github <595554963@qq.com>
2025-08-06 05:49:12 +0800

    Update MainForm.Designer.cs

--- AssetStudio.GUI/MainForm.Designer.cs
+++ AssetStudio.GUI/MainForm.Designer.cs
@@ -635,7 +635,7 @@ namespace AssetStudio.GUI
 
             loadAIToolStripMenuItem.Name = "loadAIToolStripMenuItem";
             loadAIToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
-            loadAIToolStripMenuItem.Text = "加载AI";
+            loadAIToolStripMenuItem.Text = "加载资源索引";
             loadAIToolStripMenuItem.Click += loadAIToolStripMenuItem_Click;
 
             loadCABMapToolStripMenuItem.Name = "loadCABMapToolStripMenuItem";
