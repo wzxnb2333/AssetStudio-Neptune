ac3176241bbb6466ce1fa17b8db9e3335ba5e535
595554963github <595554963@qq.com>
2025-08-05 18:33:23 +0800

    Add files via upload

---
+++ Assetstudio/LZ4/LZ4InvArknights.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AssetStudio;
+public class LZ4InvArknights : LZ4
+{
+    public new static LZ4InvArknights Instance => new();
+
+    protected override (int encCount, int litCount) GetLiteralToken(ReadOnlySpan<byte> cmp, ref int cmpPos) =>
+        ((cmp[cmpPos] >> 4) & 0xf, (cmp[cmpPos++] >> 0) & 0xf);
+
+    protected override int GetChunkEnd(ReadOnlySpan<byte> cmp, ref int cmpPos) =>
+        cmp[cmpPos++] << 8 | cmp[cmpPos++] << 0;
+}
